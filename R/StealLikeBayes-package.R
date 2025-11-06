487b5abd7581cc04fe793886542b853df448dcb8
Tomasz Woźniak <wozniak.tom@pm.me>
2025-11-06 21:57:07 +1100

    after nicepackage

---
+++ R/StealLikeBayes-package.R
@@ -0,0 +1,7 @@
+#' @name StealLikeBayes-package
+#' @aliases StealLikeBayes-package StealLikeBayes
+#' @docType package
+#' @importFrom Rcpp sourceCpp
+#' @useDynLib StealLikeBayes, .registration = TRUE
+#' @keywords internal
+'_PACKAGE'
